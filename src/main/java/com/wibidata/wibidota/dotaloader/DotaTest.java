a6ac2d2ac8649308f2092a2b2b7d504ec440061b
Chris Clark <chris@wibidata.com>
2013-07-05 20:19:46 -0700

    More file changes, add DotaTest.java

--- src/main/java/com/wibidata/wibidota/dotaloader/DotaTest.java
+++ src/main/java/com/wibidata/wibidota/dotaloader/DotaTest.java
@@ -18,14 +18,12 @@
 
 package com.wibidata.wibidota.dotaloader;
 
-import java.io.IOException;
-import java.util.Set;
-import java.util.HashSet;
-
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -40,36 +38,21 @@ import org.apache.hadoop.util.ToolRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import java.io.IOException;
+
+import java.util.Set;
+import java.util.HashSet;
 
 /**
  * A Map Reduce job built to gather statistics about the different values various fields stored
  * in raw Dota match JSON data can take. The output can be subdivided by intervals
  * of matches so we can, for instances, see if new records take on values older ones did not.
  */
-public class DotaEnumCounter extends Configured implements Tool {
-  private static final Logger LOG = LoggerFactory.getLogger(DotaEnumCounter.class);
+public class DotaTest extends Configured implements Tool {
+  private static final Logger LOG = LoggerFactory.getLogger(DotaTest.class);
 
   static enum Counters {
     MALFORMED_MATCH_LINES
-  }
-
-  // Change these fields to change behavior
-
-  // The fields in the JSON to track
-  private static final String[] MATCH_FIELDS = new String[]{"game_mode"};
-
-  // The fields in the player JSONObjects to track
-  private static final String[] PLAYER_FIELDS = new String[]{"leaver_status"};
-
-  // Interval to subdived the results by, can be null
-  private static final Integer INTERVAL = 1000000;
-
-  // Convert a potentially null object to a string
-  private static String safeToString(Object o){
-      return ( o == null ? "null" : o.toString());
   }
 
   /**
@@ -83,27 +66,20 @@ public class DotaEnumCounter extends Configured implements Tool {
     // Just to caches this value
     private static final LongWritable ONE = new LongWritable(1);
 
-    private static String toReturnKey(String field, long slot, String value){
-        return field + "::" + value +
-               (INTERVAL == null ? "" :
-               " (" + slot * INTERVAL + "-" + (slot + 1) * INTERVAL + ")");
-    }
+    private static final Set<Integer> LEAVER_STATUS = new HashSet<Integer>();
 
     public void map(LongWritable key, Text value, Context context)
         throws IOException, InterruptedException {
       try{
         JsonObject matchData = PARSER.parse(value.toString()).getAsJsonObject();
-        long seqNum = matchData.get("match_seq_num").getAsLong();
-        long slot = seqNum / INTERVAL;
-        for(String field : MATCH_FIELDS){
-          context.write(new Text(toReturnKey(field, slot, safeToString(matchData.get(field)))), ONE);
-        }
-
         for (JsonElement playerElem : matchData.get("players").getAsJsonArray()) {
           JsonObject playerData = playerElem.getAsJsonObject();
-          for(String field : PLAYER_FIELDS){
-            context.write(new Text(toReturnKey(field, slot, safeToString(playerData.get(field)))), ONE);
+          JsonElement leaverStatus = playerData.get("leaver_status");
+          int leaverStatusInt = (leaverStatus == null ? -10 : leaverStatus.getAsInt());
+          if(!LEAVER_STATUS.contains(leaverStatusInt)){
+              LOG.error("Found:" + leaverStatusInt + "\n" + value.toString());
           }
+          LEAVER_STATUS.add(leaverStatusInt);
         }
       } catch (IllegalStateException e) {
         // Indicates malformed JSON.
@@ -138,7 +114,7 @@ public class DotaEnumCounter extends Configured implements Tool {
    */
   public static void main(String args[]) throws Exception {
     Configuration conf = new Configuration();
-    int res = ToolRunner.run(conf, new DotaEnumCounter(), args);
+    int res = ToolRunner.run(conf, new DotaTest(), args);
     System.exit(res);
   }
 
@@ -148,10 +124,8 @@ public class DotaEnumCounter extends Configured implements Tool {
     job.setMapOutputValueClass(LongWritable.class);
 
     job.setMapperClass(Map.class);
-    job.setCombinerClass(Reduce.class);
-    job.setReducerClass(Reduce.class);
 
-    job.setJarByClass(DotaEnumCounter.class);
+    job.setJarByClass(DotaTest.class);
 
     job.setInputFormatClass(TextInputFormat.class);
     job.setOutputFormatClass(TextOutputFormat.class);
