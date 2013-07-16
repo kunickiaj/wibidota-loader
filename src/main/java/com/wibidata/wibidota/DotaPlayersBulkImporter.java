2a69786bf530886e44383f8113e406af22f830e7
Chris Clark <chris@wibidata.com>
2013-07-16 11:19:10 -0700

    Bug fixes

--- src/main/java/com/wibidata/wibidota/DotaPlayersBulkImporter.java
+++ src/main/java/com/wibidata/wibidota/DotaPlayersBulkImporter.java
@@ -65,7 +65,6 @@ public class DotaPlayersBulkImporter extends KijiBulkImporter<LongWritable, Text
         if(accountId != null && accountId != -1){
           otherPlayers.remove(i);
           players.setPlayers(otherPlayers);
-          LOG.info("adding: " + accountId);
           EntityId eid = context.getEntityId(accountId + "");
           context.put(eid, "data", "match_id", startTime, matchId);
           context.put(eid, "data", "dire_towers_status", startTime, direTowers);
