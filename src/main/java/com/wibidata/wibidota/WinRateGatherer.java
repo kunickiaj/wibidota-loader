0f8da5a295a054d6acaf2bdf618dd15b668b04c9
Chris Clark <chris@wibidata.com>
2013-07-16 21:44:17 -0700

    Bug fixes

---
+++ src/main/java/com/wibidata/wibidota/WinRateGatherer.java
@@ -0,0 +1,11 @@
+package com.wibidata.wibidota;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chris
+ * Date: 7/16/13
+ * Time: 9:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class WinRateGatherer {
+}
